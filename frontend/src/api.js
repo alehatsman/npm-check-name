9f9eac29e6cff2ba1665da7ab4c9d35a4cbe5459
Aleh Atsman <aleh.atsman@gmail.com>
2017-05-18 18:48:26 +0300

    feat(api): use cors proxy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 import ajax from './xhr';
 
+/*
 export default function checkNake(name) {
   if (!name) {
     throw new Error('name must be provided');
@@ -8,4 +9,15 @@ export default function checkNake(name) {
     method: 'GET',
     url: `/api/check-name/${name}`,
   });
+}
+*/
+
+export default function checkNameUsingProxy(name) {
+  if (!name) {
+    throw new Error('name must be provided');
+  }
+  return ajax({
+    method: 'GET',
+    url: `https://crossorigin.me/https://registry.npmjs.org/${name}`,
+  }).then(res => ({ result: !res._id })); // eslint-disable-line
 }
